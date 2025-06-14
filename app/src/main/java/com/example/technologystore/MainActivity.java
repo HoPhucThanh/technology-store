8a090075bc74747d80a761758d8bf617d1e22acb
HoPhucThanh <hothanhx8ql@gmail.com>
2025-06-14 22:41:16 +0700

    Add commit test comment in MainActivity.java

--- app/src/main/java/com/example/technologystore/MainActivity.java
+++ app/src/main/java/com/example/technologystore/MainActivity.java
@@ -1,26 +1,38 @@
 package com.example.technologystore;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin, btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Commit test by [your name] - 11/06/2025
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
 
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Mở trang đăng nhập
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Mở trang đăng ký
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
         });
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "technology store"
 include(":app")
- 
+
